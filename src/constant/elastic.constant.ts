509aa8b240c30aff36899c62e1bc7d6eabe0683d
Andranik Barseghyan <barsegyanat.93@gmail.com>
2021-09-12 01:25:15 +0400

    fix: connect external host(VM)

--- src/constant/elastic.constant.ts
+++ src/constant/elastic.constant.ts
@@ -1,9 +1,10 @@
 export const ELASTIC = {
-  LOCAL_HOST: 'http://localhost:9200',
-  VM_HOST: 'http://192.168.171.139:9200',
+  // HOSTS: 'localhost:9200',
+  HOSTS: '192.168.171.141:9200',
   URL: 'localhost',
   PORT: '9200',
   INDEX: 'products',
-  USERNAME: 'elastic',
+  USERNAME: 'root',
   PASSWORD: 'root',
+  PROTOCOL: 'http',
 };
